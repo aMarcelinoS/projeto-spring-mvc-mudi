839198faf540136f28fe0035203453caabca1618
Alexandre Marcelino <amsantos9@gmail.com>
2022-01-17 07:09:48 -0300

    Projeto criado

---
+++ src/main/java/br/com/example/mvc/mudi/ProjetoSpringMvcMudiApplication.java
@@ -0,0 +1,13 @@
+package br.com.example.mvc.mudi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringMvcMudiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringMvcMudiApplication.class, args);
+	}
+
+}
