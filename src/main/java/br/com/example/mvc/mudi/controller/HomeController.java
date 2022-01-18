37b8165269d502cbd2cf30fe52f053bf4f28a1a2
Alexandre Marcelino <amsantos9@gmail.com>
2022-01-18 12:00:57 -0300

    Anotatios e Injeção de dependências

--- src/main/java/br/com/example/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/example/mvc/mudi/controller/HomeController.java
@@ -2,30 +2,25 @@ package br.com.example.mvc.mudi.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.example.mvc.mudi.model.Pedido;
+import br.com.example.mvc.mudi.repository.PedidoRepository;
 
 @Controller
 public class HomeController {
 	
-	@PersistenceContext
-	private EntityManager entityManager;
+	@Autowired
+	private PedidoRepository repository;
+	
 	
 	@GetMapping("/home")
 	public String home(Model model) {
-		
-		Query query = entityManager.createQuery("SELECT p FROM Pedido p", Pedido.class);
-		List<Pedido> pedidos = query.getResultList();	
-		
+		List<Pedido> pedidos = repository.buscaTodosPedidos();
 		model.addAttribute("pedidos", pedidos);
-		
 		return "home";
 	}
 }
