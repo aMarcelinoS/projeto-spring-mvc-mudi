6c8ba6626e080efc8b5c6d816a20571312278e33
Alexandre Marcelino <amsantos9@gmail.com>
2022-01-19 21:18:53 -0300

    Implementado status do pedido

--- src/main/java/br/com/example/mvc/mudi/dto/RequisicaoNovoPedido.java
+++ src/main/java/br/com/example/mvc/mudi/dto/RequisicaoNovoPedido.java
@@ -3,6 +3,7 @@ package br.com.example.mvc.mudi.dto;
 import javax.validation.constraints.NotBlank;
 
 import br.com.example.mvc.mudi.model.Pedido;
+import br.com.example.mvc.mudi.model.StatusPedido;
 
 public class RequisicaoNovoPedido {
 
@@ -45,7 +46,8 @@ public class RequisicaoNovoPedido {
 		pedido.setDescricao(descricao);
 		pedido.setNomeProduto(nomeProduto);
 		pedido.setUrlImagem(urlImagem);
-		pedido.setUrlProduto(urlProduto);			
+		pedido.setUrlProduto(urlProduto);
+		pedido.setStatus(StatusPedido.AGUARDANDO);
 		return pedido;
 	}	
 }
